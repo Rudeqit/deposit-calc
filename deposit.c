6609eb2b91e16a3c7fe910e3a73f168c95d47e44
Rudeqit <antoshapianist@gmail.com>
2017-02-25 03:32:40 +0700

    Implement request handling

--- deposit.c
+++ deposit.c
@@ -9,9 +9,34 @@ int check(int s, int t)
 
 int main()
 {
-	int s, t;
+	int s, t, r;
 	printf("Введите сумму и срок вклада:\n");
-	scanf("%d %d", &s, &t);
-	
+	scanf("%d%d", &s, &t);
+
+	if (check(s,t) == 0)
+		printf("WRONG\n");
+
+	else {
+		if ((t >= 0) && (t <= 30)) {
+			r = s - (s*0.1);
+			printf("%d", r);
+		}
+
+		if ((t >= 31) && (t <= 120)) {
+                        r = s + (s*0.02);
+                        printf("%d", r);
+                }
+
+		if ((t >= 121) && (t <= 240)) {
+                        r = s + (s*0.06);
+                        printf("%d", r);
+                }
+
+		if ((t >= 241) && (t <= 365)) {
+                        r =s + (s*0.12);
+                        printf("%d", r);
+                }
+	}
+
 	return 0;
 }
