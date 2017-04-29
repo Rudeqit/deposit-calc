6c2a19c4b23f6a9379c39da832c6ab7f5e4f4ecd
Rudeqit <antoshapianist@gmail.com>
2017-04-29 12:44:00 +0700

    All test complit

--- test/validation_test.c
+++ test/validation_test.c
@@ -1,6 +1,57 @@
 #include <ctest.h>
 #include <deposit.h>
 
+//16
+CTEST(check, valid_input_negative_less_10k)
+{
+	const int dep = 0;
+	const int day = -1;
+	const int result = check(dep, day);
+	
+	ASSERT_FALSE(result);
+}
+
+//17
+CTEST(check, valid_input_negative_more_10k)
+{
+	const int dep = 10000;
+	const int day = -1;
+	const int result = check(dep, day);
+	
+	ASSERT_FALSE(result);
+}
+
+//18
+CTEST(check, valid_input_0_less_10k)
+{
+	const int dep = 0;
+	const int day = -1;
+	const int result = check(dep, day);
+	
+	ASSERT_FALSE(result);
+}
+
+//19
+CTEST(check, valid_input_0_more_10k)
+{
+	const int dep = 10000;
+	const int day = 0;
+	const int result = check(dep, day);
+	
+	ASSERT_TRUE(result);
+}
+
+//20
+CTEST(check, valid_input_365_less_10k)
+{
+	const int dep = 0;
+	const int day = 365;
+	const int result = check(dep, day);
+	
+	ASSERT_FALSE(result);
+}
+
+//21
 CTEST(check, valid_input_365_more_10k)
 {
 	const int dep = 10000;
@@ -10,10 +61,21 @@ CTEST(check, valid_input_365_more_10k)
 	ASSERT_TRUE(result);
 }
 
-CTEST(check, valid_input_negative_less_10k)
+//22
+CTEST(check, valid_input_366_less_10k)
 {
 	const int dep = 0;
-	const int day = -1;
+	const int day = 366;
+	const int result = check(dep, day);
+	
+	ASSERT_FALSE(result);
+}
+
+//23
+CTEST(check, valid_input_366_more_10k)
+{
+	const int dep = 10000;
+	const int day = 366;
 	const int result = check(dep, day);
 	
 	ASSERT_FALSE(result);
